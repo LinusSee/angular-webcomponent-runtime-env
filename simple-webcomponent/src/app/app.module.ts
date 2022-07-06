3049bdef3048cb67cecf5155c04e20973cec70fe
LinusSee <linussee@web.de>
2022-07-06 20:55:38 +0200

    feat: create web component in bootstrap

--- simple-webcomponent/src/app/app.module.ts
+++ simple-webcomponent/src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,14 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+
+  constructor(private injector: Injector) {}
+
+  public ngDoBootstrap(): void {
+    const customAngularElement = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('simple-webcomponent', customAngularElement);
+  }
+}
